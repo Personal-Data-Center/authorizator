374236fc56e542ab6f42be68dfec6ad1f4bb0f26
Lucas Coraça Silva <lucascoracasilva@gmail.com>
2020-10-07 02:10:47 -0300

    removed django auth session admin

--- authorizator/settings.py
+++ authorizator/settings.py
@@ -28,13 +28,15 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 
+
+# Application definition
+
+ADMIN_ENABLED = False
+
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
     'django.contrib.contenttypes',
-    'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
@@ -44,7 +46,6 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
